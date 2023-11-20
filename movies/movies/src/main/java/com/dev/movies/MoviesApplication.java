5ecdedf81a1a46e41731b4f621d0dc4fe5679536
harshithamaripally24 <98552203+harshithamaripally24@users.noreply.github.com>
2023-11-19 22:53:35 -0700

    project base setup

---
+++ movies/movies/src/main/java/com/dev/movies/MoviesApplication.java
@@ -0,0 +1,30 @@
+package com.dev.movies;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+
+public class MoviesApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(MoviesApplication.class);
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(MoviesApplication.class, args);
+
+	}
+
+
+}
